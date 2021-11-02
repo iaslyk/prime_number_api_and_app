c670dc7e80313833299c776c8ece59ff891e15ca
Oliver <oliversulyok@gmail.com>
2021-11-02 09:36:53 +0100

    Minor fixes

--- api.py
+++ api.py
@@ -15,18 +15,21 @@ def index():
 
 @app.route('/prime/<int:number>', methods=['GET'])
 def display_prime(number):
-    if number <= 1:
+    if number < 1:
         return str(bounds_error)
-
+    # Start timer
     t0 = time.time()
-    prime_numbers = [2,3]
+    prime_numbers = [2, 3]
     if number < 3:
-        return prime_numbers[:number]
+        return str(prime_numbers[:number])
     for i in range(2, number):
         next_pn = prime_numbers[-1] + 2
         while any(not(next_pn%pn) for pn in prime_numbers):
             next_pn += 2
-        prime_numbers.append(next_pn)
+        prime_numbers.append(next_pn)        
+    # End timer
+    t1 = time.time()
+    print("Execution time:", t1 - t0)
     return str(prime_numbers)
 
 if __name__ ==  "__main__":
