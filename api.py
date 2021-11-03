98e0f101dc919e4be4514f28ea65f713d02975d5
Oliver <oliversulyok@gmail.com>
2021-11-03 10:22:30 +0100

    Added changing cache timer at app startup

--- api.py
+++ api.py
@@ -1,19 +1,28 @@
 import time
 from flask import Flask
+from flask_caching import Cache
 
+config = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "FileSystemCache",  # Flask-Caching related configs
+    'CACHE_DIR': '/tmp'
+}
 
 app = Flask(__name__)
-
+app.config.from_mapping(config)
+cache = Cache(app)
 
 welcome_message = "Welcome! Enter the url/prime/int to see your list of prime numbers."
 bounds_error = "The number you entered is out of bounds."
+cache_time = input("Enter cache time (in seconds): ")
+cache_time = int(cache_time)
 
 @app.route('/')
 def index():
     return str(welcome_message)
 
-
-@app.route('/prime/<int:number>', methods=['GET'])
+@app.route('/prime/<int:number>', methods=['GET', 'POST'])
+@cache.cached(timeout=cache_time)
 def display_prime(number):
     if number < 1:
         return str(bounds_error)
